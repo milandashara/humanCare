7bbea40434f89fc567ca415af3e1e571f69e4514
milandashara <milandashara@gmail.com>
2014-05-07 18:55:32 +0800

    data layer

---
+++ HumanCare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HumanCare.MvcApplication" Language="C#" %>
---
+++ dataAccessLayer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dataAccessLayer
+{
+    public class Class1
+    {
+    }
+}
